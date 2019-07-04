6ea743520e1fca2533929291245a8f23c26aa2f6
Marius Grondahl <marius@grondahldesign.no>
2019-07-04 16:12:16 +0200

    First commit

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,38 @@
 class MemoryGame {
   constructor(card){
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+
+
 }
+
+MemoryGame.prototype.shuffleCards = function() {
+  let currentIndex = arr.length;
+  let randomIndex = 0;
+  let currentValue = 0;
+
+  while (currentIndex > 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    currentValue = arr[currentIndex];
+    
+    arr[currentIndex] = arr[randomIndex];
+    arr[randomIndex] = currentValue;
+  }
+  return arr;
+};
+
+MemoryGame.prototype.checkIfPair = function(firstCard, secondCard) {
+
+};
+
+
+MemoryGame.prototype.isFinished = function(firstCard, secondCard) {
+
+};
+
+
+// let newGame = new MemoryGame {};
